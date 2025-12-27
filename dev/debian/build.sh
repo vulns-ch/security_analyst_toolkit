7a4510779ffc298793618b1a48909e1f54ccf75c
Toni Tanner <toni@comp370.localdomain>
2025-12-27 18:15:27 +0100

    scripts as ext2

--- dev/debian/build.sh
+++ dev/debian/build.sh
@@ -2,9 +2,11 @@ docker buildx build --platform linux/i386 -t sat_debian .
 docker rm sat_debian
 docker create --name sat_debian sat_debian
 rm -rf ext2
+rm -rf ext2_scripts
 rm *.ext2
 rm -rf split
 mkdir ext2
+mkdir ext2_scripts
 mkdir split
 docker cp sat_debian:/ ext2/
 /sbin/mkfs.ext2 -b 4096 -d ext2/ debian.ext2 300M
@@ -18,5 +20,6 @@ echo "created $index_list";
 rm -rf ../../docs/disk-images/debian
 cp -r split ../../docs/disk-images/debian
 cp -r debian.ext2 ../../docs/disk-images/
-
-
+cp -r scripts/* ext2_scripts
+/sbin/mkfs.ext2 -b 4096 -d ext2_scripts/ scripts.ext2 5M
+cp scripts.ext2 ../../docs/disk-images/
